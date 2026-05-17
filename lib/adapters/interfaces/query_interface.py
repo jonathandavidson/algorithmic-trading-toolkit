e1778806fd80ca535a2b57e867edf21581047fe8
Jonathan Davidson <jdavidson@netsplasher.com>
2026-05-17 10:21:21 -0500

    Rename QueryInterface to DatasourceQueryInterface in adapters/interfaces

--- lib/adapters/interfaces/query_interface.py
+++ lib/adapters/interfaces/query_interface.py
@@ -5,7 +5,7 @@ from lib.models.historical_bars import BaseModel
 from lib.services.configuration.collection import CollectionConfiguration
 
 
-class QueryInterface(ABC):
+class DatasourceQueryInterface(ABC):
 
     @abstractmethod
     def fetch_rows(self, collection_config: CollectionConfiguration) -> Generator[list[BaseModel], None, None]: ...

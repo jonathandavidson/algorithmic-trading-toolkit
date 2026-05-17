7619521fe211effc10c9f26bf1fd26d1997a4c7b
Jonathan Davidson <jdavidson@netsplasher.com>
2026-05-17 09:55:12 -0500

    Add QueryInterface with fetch_rows abstract method

---
+++ lib/adapters/interfaces/query_interface.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from collections.abc import Generator
+
+from lib.models.historical_bars import BaseModel
+from lib.services.configuration.collection import CollectionConfiguration
+
+
+class QueryInterface(ABC):
+
+    @abstractmethod
+    def fetch_rows(self, collection_config: CollectionConfiguration) -> Generator[list[BaseModel], None, None]: ...
